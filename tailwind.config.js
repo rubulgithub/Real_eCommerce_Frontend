268707740e639811064c8df8d227dc326bbf8c15
Rubul Islam <114800261+rubulgithub@users.noreply.github.com>
2025-02-02 15:59:47 +0530

    work on home page and responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+// tailwind.config.js
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
+    },
+    extend: {
+      screens: {
+        xs: "400px",
+      },
+    },
   },
   plugins: [],
 };
