2984498979b498d8c228e4b6c8f21f00ce1a2fb8
Rubul Islam <114800261+rubulgithub@users.noreply.github.com>
2025-03-08 15:03:01 +0530

    log

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -5,8 +5,6 @@ import { useNavigate } from "react-router-dom";
 function AuthLayout({ children, authentication }) {
   const navigate = useNavigate();
   const authStatus = useSelector((state) => state.auth.status);
-  console.log("one");
-  console.log("auth from Authlayout", authStatus);
 
   useEffect(() => {
     if (authentication && authStatus !== authentication) {
